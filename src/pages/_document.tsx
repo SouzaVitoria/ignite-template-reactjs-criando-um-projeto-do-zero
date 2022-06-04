a1273fc274971a3658c6383a00b3b697bf881741
Vitória Souza <viihsouza161@gmail.com>
2022-06-04 14:26:08 -0300

    added global styles and font-family

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,10 @@
-import Document from 'next/document';
+import { Head } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render() {
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
+    </Head>
+  }
 }
