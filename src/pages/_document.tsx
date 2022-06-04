d1735b3f525a0c9749691de49b698c0a7d170f49
Vitória Souza <viihsouza161@gmail.com>
2022-06-04 15:08:19 -0300

    added _document file structure

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,18 @@
-import { Head } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
-    <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
-    </Head>
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
