adbae5c2fab1fb3c96d995f54d195bb2fd05ee0e
Vitória Souza <viihsouza161@gmail.com>
2022-06-07 07:58:57 -0300

    added posts page and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next';
 import { getPrismicClient } from '../services/prismic';
+import { FiCalendar, FiUser } from "react-icons/fi"
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -25,56 +26,65 @@ interface HomeProps {
 
 export default function Home({ postsPagination }: HomeProps) {
   return (
-    <div>
-      {/* {
-        postsPagination.results.map(post => {
-          return (
-            <>
-              <h1>{post.data.title}</h1>
-              <h3>{post.data.subtitle}</h3>
-            </>
-          )
-        })
-      } */}
+    <div className={styles.container}>
+      <div className={styles.postsContainer}>
+        {
+          postsPagination.results.map(post => {
+            return (
+              <div key={post.uid} className={styles.content}>
+                <h1 className={styles.title}>{post.data.title}</h1>
+                <h3 className={styles.subtitle}>{post.data.subtitle}</h3>
+                <div className={styles.dateAndAuthor}>
+                  <span className={styles.date}>
+                    <FiCalendar size={20} />
+                    {post.first_publication_date}
+                  </span>
+                  <span className={styles.author}>
+                    <FiUser size={20} />
+                    {post.data.author}
+                  </span>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient({})
-  const response = await prismic.getByType('post', {
+  const postsResponse = await prismic.getByType('post', {
     pageSize: 1,
   });
-  console.log(response)
 
-  // const posts: Post[] = postsResponse.results.map((post: Post) => {
-  //   const { uid, first_publication_date } = post
-  //   const { title, subtitle, author } = post.data
-  //   const createdAt = new Date(first_publication_date).toLocaleDateString("pt-BR", {
-  //     day: "2-digit",
-  //     month: "short",
-  //     year: "numeric"
-  //   })
+  const posts: Post[] = postsResponse.results.map((post: Post) => {
+    const { uid, first_publication_date, data: { title, subtitle, author } } = post
+    const createdAt = new Date(first_publication_date).toLocaleDateString("pt-BR", {
+      day: "2-digit",
+      month: "short",
+      year: "numeric"
+    })
 
-  //   return {
-  //     uid,
-  //     first_publication_date: createdAt,
-  //     data: {
-  //       title,
-  //       subtitle,
-  //       author
-  //     }
-  //   }
-  // })
+    return {
+      uid,
+      first_publication_date: createdAt,
+      data: {
+        title,
+        subtitle,
+        author
+      }
+    }
+  })
 
-  // const postsPagination: PostPagination = {
-  //   next_page: postsResponse.next_page,
-  //   results: posts,
-  // }
+  const postsPagination = {
+    results: posts,
+  }
 
   return {
     props: {
-
+      postsPagination
     }
   }
 };
