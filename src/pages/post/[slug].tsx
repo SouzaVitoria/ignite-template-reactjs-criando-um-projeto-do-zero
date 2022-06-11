96c8a853b43405f0779548e1a2f88ae6ad53953f
vitoria-souza_belcorp <vitoria.souza@acct.global>
2022-06-11 13:29:01 -0300

    added post page content and styles

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,5 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { RichText } from 'prismic-dom';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -28,9 +29,36 @@ interface PostProps {
 }
 
 export default function Post({ post }: PostProps) {
+  const { first_publication_date, data: { title, banner: { url }, author, content } } = post
 
   return (
-    <h1> {post.data.title} </h1>
+    <div className={styles.container}>
+      <img src={url} alt={title} className={styles.full} />
+      <div className={commonStyles.content}>
+        <h1 className={`${commonStyles.title} ${styles.title}`}> {title} </h1>
+        <div className={commonStyles.info}>
+          <span className={commonStyles.date}>
+            <FiCalendar size={20} />
+            {first_publication_date}
+          </span>
+          <span className={commonStyles.author}>
+            <FiUser size={20} />
+            {author}
+          </span>
+        </div>
+        <main className={styles.content}>
+          {content.map(current => {
+            return (
+              <>
+                <h2 className={styles.subtitles}> {current.heading} </h2>
+                <div dangerouslySetInnerHTML={{ __html: current.body }} />
+              </>
+            )
+          })}
+        </main>
+      </div>
+
+    </div>
   )
 }
 
